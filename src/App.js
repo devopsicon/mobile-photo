ddfd4140992205526cc1cb1bd80616e4f5a6e81b
Daniel Yinanc <noemails@noemails.com>
2019-01-30 07:42:32 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
+  onTakePhoto (dataUri) {
+    let formData = new FormData();
+    formData.append('image', dataUri);
+
+    axios({
+        method: 'post',
+        url: `https://shielded-stream-30532.herokuapp.com/api/image`,
+        headers: {
+            'Content-Type': 'text/plain'
+        }, 
+        data: dataUri
+      });
+    
+    }
+
+  onCameraError (error) {
+    console.error('onCameraError', error);
+  }
+
+  onCameraStart (stream) {
+    console.log('onCameraStart');
+  }
+
+  onCameraStop () {
+    console.log('onCameraStop');
+  }
+
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <div className="Camera">
+        <Camera
+          onTakePhoto = { (dataUri) => { this.onTakePhoto(dataUri); } }
+          onCameraError = { (error) => { this.onCameraError(error); } }
+          idealFacingMode = {FACING_MODES.ENVIRONMENT}
+          idealResolution = {{width: 640, height: 480}}
+          imageType = {IMAGE_TYPES.JPG}
+          imageCompression = {0.97}
+          isMaxResolution = {false}
+          isImageMirror = {false}
+          isDisplayStartCameraError = {true}
+          sizeFactor = {1}
+          onCameraStart = { (stream) => { this.onCameraStart(stream); } }
+          onCameraStop = { () => { this.onCameraStop(); } }
+        />
+        </div>
       </div>
     );
   }
